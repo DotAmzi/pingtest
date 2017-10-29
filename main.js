14a4903ea6e1fdbcf54921ea65bd648db9b1a555
Camilo Fernandes <camilo.analista@gmail.com>
2017-10-29 19:14:43 -0200

    reorder files

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/icon.png'})
+
+  win.loadURL(`file://${__dirname}/pages/ping.html`)
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
